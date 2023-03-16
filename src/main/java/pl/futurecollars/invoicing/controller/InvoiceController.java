080cc5469c23814497d75f3cc37c41eef291578e
Amelia Kotłowska <amelia.kotlowska.ak@gmail.com>
2023-03-16 21:08:24 +0100

    konsultacja 16.03.2023

--- src/main/java/pl/futurecollars/invoicing/controller/InvoiceController.java
+++ src/main/java/pl/futurecollars/invoicing/controller/InvoiceController.java
@@ -1,16 +1,53 @@
 package pl.futurecollars.invoicing.controller;
 
+import java.util.List;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.futurecollars.invoicing.model.Invoice;
+import pl.futurecollars.invoicing.service.InvoiceService;
 
 @RestController
+@AllArgsConstructor
+@RequestMapping("/invoice")
 public class InvoiceController {
+  private final InvoiceService invoiceService;
 
-  @GetMapping("/hello")
-  public String helloWorld(@RequestParam(defaultValue = "Alan") String name){
-    return String.format("Hello %s", name);
+  @GetMapping
+  public List<Invoice> getInvoices() {
+    return invoiceService.getAll();
   }
 
+  @PostMapping
+  public int addInvoice(@RequestBody Invoice invoice) {
+    return invoiceService.save(invoice);
+  }
 
+  @GetMapping("/{id}")
+  public ResponseEntity<Invoice> getInvoiceById(@PathVariable int id) {
+    return invoiceService.getById(id)
+        .map(invoice -> ResponseEntity.ok().body(invoice))
+        .orElse(ResponseEntity.notFound().build());
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<?> deleteById(@PathVariable int id) {
+    return invoiceService.delete(id)
+        .map(name -> ResponseEntity.noContent().build())
+        .orElse(ResponseEntity.notFound().build());
+  }
+
+  @PutMapping("{id}")
+  public ResponseEntity<?> updateById(@PathVariable int id, @RequestBody Invoice invoice) {
+    return invoiceService.update(id, invoice)
+        .map(name -> ResponseEntity.noContent().build())
+        .orElse(ResponseEntity.notFound().build());
+  }
 }
