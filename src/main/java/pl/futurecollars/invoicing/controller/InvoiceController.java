4e534b11d606479084bd2148add39dbae7b8e499
Amelia Kotłowska <amelia.kotlowska.ak@gmail.com>
2023-03-16 17:43:30 +0100

    lekcja

--- src/main/java/pl/futurecollars/invoicing/controller/InvoiceController.java
+++ src/main/java/pl/futurecollars/invoicing/controller/InvoiceController.java
@@ -1,13 +1,16 @@
 package pl.futurecollars.invoicing.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class InvoiceController {
 
   @GetMapping("/hello")
-  public String helloWorld(){
-    return "Hello world";
+  public String helloWorld(@RequestParam(defaultValue = "Alan") String name){
+    return String.format("Hello %s", name);
   }
+
+
 }
---
+++ src/main/java/pl/futurecollars/invoicing/model/Name.java
@@ -0,0 +1,13 @@
+package pl.futurecollars.invoicing.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Name {
+  Integer id;
+  String name;
+}
