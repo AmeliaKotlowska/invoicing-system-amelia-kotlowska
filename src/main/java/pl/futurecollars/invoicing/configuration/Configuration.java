3a8491a1f1e8452a4f1503f2726c6270fd5e64f0
Amelia Kotłowska <amelia.kotlowska.ak@gmail.com>
2023-03-30 16:42:03 +0200

    Lesson 8 30.03.2023

--- src/main/java/pl/futurecollars/invoicing/configuration/Configuration.java
+++
@@ -1,8 +0,0 @@
-package pl.futurecollars.invoicing.configuration;
-
-public class Configuration {
-
-  public static final String INVOICES_FILE_PATH = "db/invoices.json";
-
-  public static final String LAST_ID_FILE_PATH = "db/lastId.txt";
-}
