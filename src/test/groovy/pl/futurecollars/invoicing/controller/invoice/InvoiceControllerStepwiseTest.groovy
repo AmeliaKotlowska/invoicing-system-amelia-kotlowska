852a6e95469d31282adae09c8a9e042b368273f3
AmeliaKotlowska <amelia.kotlowska.ak@gmail.com>
2023-08-25 15:03:52 +0200

    Lesson 14

--- src/test/groovy/pl/futurecollars/invoicing/controller/invoice/InvoiceControllerStepwiseTest.groovy
+++ src/test/groovy/pl/futurecollars/invoicing/controller/invoice/InvoiceControllerStepwiseTest.groovy
@@ -1,12 +1,16 @@
 package pl.futurecollars.invoicing.controller.invoice
 
+import com.mongodb.client.MongoDatabase
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.ApplicationContext
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
+import pl.futurecollars.invoicing.helpers.TestHelpers
 import pl.futurecollars.invoicing.model.Invoice
 import pl.futurecollars.invoicing.utils.JsonService
+import spock.lang.Requires
 import spock.lang.Shared
 import spock.lang.Specification
 import spock.lang.Stepwise
@@ -18,12 +22,19 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
-import static pl.futurecollars.invoicing.helpers.TestHelpers.invoice
+
 
 @Stepwise
 @AutoConfigureMockMvc
 @SpringBootTest
 class InvoiceControllerStepwiseTest extends Specification {
+
+    private final Invoice originalInvoice = TestHelpers.invoice(1)
+
+    private final LocalDate updatedDate = LocalDate.of(2020, 02, 28)
+
+    @Shared
+    private int invoiceId
 
     @Autowired
     private MockMvc mockMvc
@@ -31,12 +42,15 @@ class InvoiceControllerStepwiseTest extends Specification {
     @Autowired
     private JsonService jsonService
 
-    private final Invoice originalInvoice = invoice(1)
+    @Autowired
+    private ApplicationContext context
 
-    private final LocalDate updatedDate = LocalDate.of(2020, 02, 28)
-
-    @Shared
-    private int invoiceId
+    @Requires({ System.getProperty('spring.profiles.active', 'memory').contains("mongo") })
+    def "database is dropped to ensure clean state"() {
+        expect:
+        MongoDatabase mongoDatabase = context.getBean(MongoDatabase)
+        mongoDatabase.drop()
+    }
 
     def 'empty array is returned when no invoices were created'() {
         when:
